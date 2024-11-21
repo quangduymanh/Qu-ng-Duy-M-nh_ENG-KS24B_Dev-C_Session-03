cceee1a535b99db5852934ae3e94138e920350bc
quangduymanh <quangduymanh4@gmail.com>
2024-11-21 21:43:12 +0700

    Update session 03- bài 08.cpp

--- session 03- bài 08.cpp
+++ session 03- bài 08.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int number, reversedNumber = 0;
+    int number, reversedNumber;
 
-    // Yeu cau nguoi dung nhap so nguyen co 4 chu so
+    // Nhap mot so nguyen co 4 chu so
     printf("Nhap mot so nguyen co 4 chu so: ");
     scanf("%d", &number);
 
-    // Kiem tra xem so nhap co phai la so 4 chu so khong
+    // Kiem tra xem so co phai la so 4 chu so hay khong
     if (number < 1000 || number > 9999) {
         printf("So nhap vao khong phai la so 4 chu so.\n");
         return 1;
     }
 
     // Dao nguoc so
-    while (number != 0) {
-        int digit = number % 10;          // Lay chu so cuoi cung
-        reversedNumber = reversedNumber * 10 + digit;  // Them chu so vao so nghich dao
-        number /= 10;  // Loai bo chu so cuoi cung khoi so
-    }
+    int hangNghin = number / 1000;       // Lay chu so hang nghin
+    int hangTram = (number / 100) % 10; // Lay chu so hang tram
+    int hangChuc = (number / 10) % 10;  // Lay chu so hang chuc
+    int hangDonVi = number % 10;        // Lay chu so hang đon vi
+
+    // Tạo số nghịch đảo
+    reversedNumber = hangDonVi * 1000 + hangChuc * 100 + hangTram * 10 + hangNghin;
 
     // In ra so nghich dao
     printf("So nghich dao la: %d\n", reversedNumber);
